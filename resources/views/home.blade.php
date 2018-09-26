d25f48e506d8e440c9850bf44e5f136ab74dc916
Sander de Vos <sander@tutanota.de>
2018-09-26 15:11:27 +0200

    Hero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row mt-5">
+        <div class="col-md-8 hero text-center text-md-left">
+            <p class="lead mb-0">Hallo, mijn naam is</p>
+            <h1 class="display-1 mb-2"><strong>Sander</strong> de Vos</h1>
+            <p class="lead">Software engineer, DevOps en informatiebeveiliging.</p>
         </div>
     </div>
 </div>
