5e5f3f1b36b59d04b36ae17d36a3ab2c8a150a85
Sander de Vos <sander@tutanota.de>
2018-09-28 13:54:44 +0200

    Internships added to experience

--- resources/views/components/experience.blade.php
+++ resources/views/components/experience.blade.php
@@ -5,7 +5,7 @@
                 <h2 class="mb-3">Dit is wat ik heb gedaan:</h2>
             </div>
             <div class="col-md-8">
-                <div class="menu shadow-lg">
+                <div class="menu shadow-lg mb-1">
                     <div class="item p-3">
                         <div class="row">
                             <div class="col-6 col-md-3 mb-2 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 order-first">
@@ -38,7 +38,40 @@
                             </div>
                         </div>
                     </div>
+                    <div class="item p-3">
+                        <div class="row">
+                            <div class="col-6 col-md-3 mb-2 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 order-first">
+                                <img src="{{ asset('img/grrr.png') }}" class="img-fluid" alt="GRRR logo">
+                            </div>
+                            <div class="col-md-5 order-3">
+                                <h5 class="mb-0">
+                                    Stage<br>
+                                    <small class="text-muted">GRRR</small>
+                                </h5>
+                            </div>
+                            <div class="col-6 col-md-4 order-2 order-sm-3 order-md-3 order-lg-3 order-xl-3">
+                                <p class="text-right">2013 - 2014</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item p-3">
+                        <div class="row">
+                            <div class="col-6 col-md-3 mb-2 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 order-first">
+                                <img src="{{ asset('svg/driebit.svg') }}" class="img-fluid" alt="Driebit logo">
+                            </div>
+                            <div class="col-md-5 order-3">
+                                <h5 class="mb-0">
+                                    Stage<br>
+                                    <small class="text-muted">Driebit</small>
+                                </h5>
+                            </div>
+                            <div class="col-6 col-md-4 order-2 order-sm-3 order-md-3 order-lg-3 order-xl-3">
+                                <p class="text-right">2013</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <p class="mb-0">PS Ik heb ook nog wat schoolgerelateerde dingen gedaan, maar wie is er aan het tellen? 🤷‍♂️</p>
             </div>
         </div>
     </div>
