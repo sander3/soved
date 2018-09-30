236f8d37387d115c0534b57a6498a720e64cd403
Sander de Vos <sander@tutanota.de>
2018-10-01 00:19:29 +0200

    Responsive animated preloader

---
+++ resources/views/components/preloader.blade.php
@@ -0,0 +1,23 @@
+<div class="preloader position-fixed d-flex">
+    <div class="text-wrapper position-absolute d-flex align-items-center">
+        <div class="container">
+            <div class="row no-gutters justify-content-start justify-content-sm-center text-white">
+                <div class="col-12 col-sm-auto text-mask">
+                    <h3 class="my-0">Software engineer,</h3>
+                </div>
+                <div class="col-12 col-sm-auto text-mask">
+                    <h3 class="my-0">DevOps</h3>
+                </div>
+                <div class="col-auto text-mask">
+                    <h3 class="my-0">en informatiebeveiliging.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @for ($i = 0; $i < 4; $i++)
+        <div class="panel-mask position-relative w-25">
+            <div class="panel position-absolute"></div>
+        </div>
+    @endfor
+</div>
