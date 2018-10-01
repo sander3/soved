aa405238d0dc9eba7fb585339441304927cbf339
Sander de Vos <sander@tutanota.de>
2018-10-01 20:39:29 +0200

    CSP policy

---
+++ app/Policies/CspPolicy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Policies;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
+
+class CspPolicy extends Basic
+{
+    public function configure()
+    {
+        parent::configure();
+
+        $this
+            ->addDirective(Directive::STYLE, 'https://fonts.googleapis.com')
+            ->addDirective(Directive::FONT, 'https://fonts.gstatic.com');
+    }
+}
