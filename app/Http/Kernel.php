5c8fc9fec2c8f9ff69b048927a2ca16c4a3c90c6
Sander de Vos <sander@tutanota.de>
2018-10-01 20:51:46 +0200

    Feature policy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Spatie\Csp\AddCspHeaders::class,
+            \Mazedlx\FeaturePolicy\AddFeaturePolicyHeaders::class,
         ],
 
         'api' => [
---
+++ app/Policies/FeaturePolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use Mazedlx\FeaturePolicy\Value;
+use Mazedlx\FeaturePolicy\Directive;
+use Mazedlx\FeaturePolicy\Policies\Basic;
+
+class FeaturePolicy extends Basic
+{
+    public function configure()
+    {
+        $this
+            ->addDirective(Directive::ACCELEROMETER, Value::NONE)
+            ->addDirective(Directive::AMBIENT_LIGHT_SENSOR, Value::NONE)
+            ->addDirective(Directive::AUTOPLAY, Value::NONE)
+            ->addDirective(Directive::CAMERA, Value::NONE)
+            ->addDirective(Directive::ENCRYPTED_MEDIA, Value::NONE)
+            ->addDirective(Directive::FULLSCREEN, Value::NONE)
+            ->addDirective(Directive::GEOLOCATION, Value::NONE)
+            ->addDirective(Directive::GYROSCOPE, Value::NONE)
+            ->addDirective(Directive::MAGNETOMETER, Value::NONE)
+            ->addDirective(Directive::MICROPHONE, Value::NONE)
+            ->addDirective(Directive::MIDI, Value::NONE)
+            ->addDirective(Directive::PAYMENT, Value::NONE)
+            ->addDirective(Directive::PICTURE_IN_PICTURE, Value::NONE)
+            ->addDirective(Directive::SPEAKER, Value::NONE)
+            ->addDirective(Directive::USB, Value::NONE)
+            ->addDirective(Directive::VR, Value::NONE);
+    }
+}
