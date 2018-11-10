96c81e400c71804e93d4e98d20369bc8565fa986
Sander de Vos <sander@tutanota.de>
2018-11-10 21:22:59 +0100

    Matomo analytics added, dependencies updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,5 +27,18 @@
     <div id="app">
         @yield('content')
     </div>
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      _paq.push(['disableCookies']);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//matomo.sander.sh/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
 </body>
 </html>
