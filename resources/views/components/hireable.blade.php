a7169ea2fd9430dc079080f41dc6175d598a2f88
Sander de Vos <sander@tutanota.de>
2018-11-30 16:55:44 +0100

    Job search alert added

---
+++ resources/views/components/hireable.blade.php
@@ -0,0 +1,9 @@
+<div class="hireable py-1 shadow">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-md-center">
+                <p class="lead mb-0">@lang('hireable.searching')</p>
+            </div>
+        </div>
+    </div>
+</div>
