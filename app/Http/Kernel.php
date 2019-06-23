148c30dd6ddbe9ed0ebf772b3696eb647db42611
Sander de Vos <sander@tutanota.de>
2019-06-23 13:51:28 +0200

    Laravel Nova

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,19 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+
+        'web+' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\SetPreferredLocale::class,
+            // \Spatie\Csp\AddCspHeaders::class,
+            \Mazedlx\FeaturePolicy\AddFeaturePolicyHeaders::class,
+        ],
     ];
 
     /**
