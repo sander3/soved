424852ebd1e048da3dc1445beaec22a15b3ed99b
Sander de Vos <sander@tutanota.de>
2019-07-14 19:47:46 +0200

    Vacancy score changed to decimal

---
+++ database/migrations/2019_07_14_193837_modify_score_of_job_hunt_vacancies_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyScoreOfJobHuntVacanciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->dropColumn();
+
+        Schema::table('job_hunt_vacancies', function (Blueprint $table) {
+            $table
+                ->decimal('score', 2, 1)
+                ->storedAs('(age_score + culture_score + requirements_score + stack_score + IF(remote, 5, 1)) / 5')
+                ->after('stack_score');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->dropColumn();
+
+        Schema::table('job_hunt_vacancies', function (Blueprint $table) {
+            $table
+                ->tinyInteger('score')
+                ->storedAs('(age_score + culture_score + requirements_score + stack_score + IF(remote, 5, 1)) / 5')
+                ->after('stack_score');
+        });
+    }
+
+    public function dropColumn()
+    {
+        Schema::table('job_hunt_vacancies', function (Blueprint $table) {
+            $table->dropColumn('score');
+        });
+    }
+}
