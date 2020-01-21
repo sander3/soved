709f183790199cc3823558f167a943627759a246
Sander de Vos <15908302+sander3@users.noreply.github.com>
2020-01-21 10:16:43 +0100

    New quote who dis

--- resources/views/components/quote.blade.php
+++ resources/views/components/quote.blade.php
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-auto">
                 <blockquote class="blockquote mb-0">
-                    <p class="mb-0">“We hebben met z’n allen als samenleving een tocht door de woestijn gemaakt.”</p>
+                    <p class="mb-0">“Developer accused of unreadable code refuses to comment.”</p>
                     <footer class="blockquote-footer text-right">@sdevos16 on Twitter</footer>
                 </blockquote>
             </div>
