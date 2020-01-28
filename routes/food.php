87a8e02988e436eb70db0ae99f7791327976aa87
Sander de Vos <sander@tutanota.de>
2020-01-29 00:38:37 +0100

    Proof of concept

--- routes/food.php
+++ routes/food.php
@@ -1,1 +1,21 @@
 <?php
+
+use App\User;
+use App\Food\Recipe;
+use Illuminate\Database\Eloquent\Builder;
+
+Route::get('/', function () {
+    $user = User::first();
+
+    $inventory = $user->ingredients;
+
+    $recipes = Recipe::query()
+        ->with('ingredients')
+        ->whereDoesntHave('ingredients', function (Builder $query) use ($inventory) {
+            $query
+                ->whereNotIn('ingredients.id', $inventory->pluck('id'));
+        })
+        ->get();
+
+    dd($recipes);
+});
