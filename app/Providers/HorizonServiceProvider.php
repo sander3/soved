a3a6d2b1cafcff274d11533b634469f4c41f457c
Sander de Vos <sander@tutanota.de>
2020-03-21 16:50:18 +0100

    Laravel Horizon v4.0.2

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Horizon\Horizon;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+
+        $morning = today()->hour(6);
+        $evening = today()->hour(18);
+
+        if (! now()->between($morning, $evening)) {
+            Horizon::night();
+        }
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewHorizon', function ($user) {
+            return in_array($user->email, [
+                'sander@tutanota.de',
+            ]);
+        });
+    }
+}
